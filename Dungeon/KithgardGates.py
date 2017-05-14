152a37731a8ad035ff50ed61f6b6e8f3facf29e1
a-piter <amanda.piter@gmail.com>
2017-05-14 18:50:17 -0400

    Create KithgardGates.py

---
+++ Dungeon/KithgardGates.py
@@ -0,0 +1,9 @@
+# Build 3 fences to keep the ogres at bay!
+
+hero.moveDown()
+hero.buildXY("fence", 36, 34)
+hero.buildXY("fence", 35, 31)
+hero.buildXY("fence", 36, 28)
+hero.moveRight()
+hero.moveRight()
+hero.moveRight()
