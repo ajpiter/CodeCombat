8ba3c9c11922bde8436cab34484f50142ad20b94
a-piter <amanda.piter@gmail.com>
2017-05-14 18:31:02 -0400

    Create MasterOfNames.py

---
+++ Dungeon/MasterOfNames.py
@@ -0,0 +1,14 @@
+# Your hero doesn't know these enemy's names!
+# The glasses give you the findNearestEnemy ability.
+
+enemy1 = hero.findNearestEnemy()
+hero.attack(enemy1)
+hero.attack(enemy1)
+
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+enemy3 = hero.findNearestEnemy()
+hero.attack(enemy3)
+hero.attack(enemy3)
