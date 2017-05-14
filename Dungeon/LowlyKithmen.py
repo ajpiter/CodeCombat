32f6156b97fdb433517ca3bec98f1220eb5844da
a-piter <amanda.piter@gmail.com>
2017-05-14 18:31:51 -0400

    Create LowlyKithmen.py

---
+++ Dungeon/LowlyKithmen.py
@@ -0,0 +1,10 @@
+# Create a second variable and attack it!
+
+enemy1 = hero.findNearestEnemy()
+hero.attack(enemy1)
+hero.attack(enemy1)
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+hero.moveDown()
+hero.moveRight(2)
