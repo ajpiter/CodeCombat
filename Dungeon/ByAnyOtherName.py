46beccf83adbd92f160973adfbb5f1cd6de79c0a
a-piter <amanda.piter@gmail.com>
2017-05-14 18:32:32 -0400

    Create ByAnyOtherName.py

---
+++ Dungeon/ByAnyOtherName.py
@@ -0,0 +1,15 @@
+# Change the names of variables to make gems appear!
+# Change the name of variable "enemy1" to "chunky".
+chunky = hero.findNearestEnemy()
+hero.attack(chunky)
+hero.attack(chunky)
+
+# Change the name of variable "enemy2" to "monkey".
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+# Collect the gems after changing the variables.
+hero.moveUp()
+hero.moveRight()
+hero.moveRight()
