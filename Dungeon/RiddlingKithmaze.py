f1be292117e5ef8ad5caf13f42b6317d83c76ac9
a-piter <amanda.piter@gmail.com>
2017-05-14 17:58:20 -0400

    Create RiddlingKithmaze.py

---
+++ Dungeon/RiddlingKithmaze.py
@@ -0,0 +1,8 @@
+# Loops are a better way of doing repetitive things.
+
+while True:
+    # Add commands in here to repeat.
+    hero.moveRight()
+    hero.moveDown()
+    hero.moveRight(2)
+    hero.moveUp()
