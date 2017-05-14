8733db245dfd223dfaafde2b691b05706af17a29
a-piter <amanda.piter@gmail.com>
2017-05-14 17:52:45 -0400

    Create Banefire.py

---
+++ Dungeon/Banefire.py
@@ -0,0 +1,7 @@
+# The ogre looks big and slow. Movement will help you avoid his deadly attacks.
+hero.moveRight(2)
+enemy = hero.findNearestEnemy()
+hero.attack("Grumus")
+hero.moveLeft()
+hero.moveUp()
+hero.moveRight()
