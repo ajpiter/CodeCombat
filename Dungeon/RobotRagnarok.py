c3dfb6cde0a7d1ba0dbf4c15cac5b3cc63709652
a-piter <amanda.piter@gmail.com>
2017-05-14 19:19:23 -0400

    Create RobotRagnarok.py

---
+++ Dungeon/RobotRagnarok.py
@@ -0,0 +1,15 @@
+# The robot ragnarok is upon us!
+# Dodge the incoming plasma balls for 15 seconds.
+
+hero.moveUp()
+hero.moveUp()
+hero.moveDown()
+hero.moveDown()
+hero.moveLeft()
+hero.moveRight()
+hero.moveDown()
+hero.moveDown()
+hero.moveRight()
+hero.moveUp()
+hero.moveDown()
+hero.moveUp()
