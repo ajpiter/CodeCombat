c7a1fd1e74761695d191a1bda18b9237c5742596
a-piter <amanda.piter@gmail.com>
2017-05-14 13:09:23 -0400

    Create 17.MedicialAttention.py

---
+++ Desert/17.MedicialAttention.py
@@ -0,0 +1,15 @@
+# Ask the healer for help when you're under one-half health.
+
+while True:
+    currentHealth = hero.health
+    healingThreshold = hero.maxHealth / 2
+    # If your current health is less than the threshold,
+    # move to the healing point and say, "heal me".
+    # Otherwise, attack. You'll need to fight hard!
+    if currentHealth < healingThreshold:
+        hero.moveXY(65, 46)
+        hero.say("heal me")
+    else:
+        enemy = hero.findNearestEnemy()
+        if enemy:
+            hero.attack(enemy)
