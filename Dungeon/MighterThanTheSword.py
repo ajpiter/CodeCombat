e036311728e4977ebcd14a4cf045963c3ad34883
a-piter <amanda.piter@gmail.com>
2017-05-14 18:03:42 -0400

    Create MighterThanTheSword.py

---
+++ Dungeon/MighterThanTheSword.py
@@ -0,0 +1,17 @@
+# A variable is a way of holding on to a value.
+# Here the "password" variable holds the secret phrase we need.
+password = 'Secret Message'
+hero.moveUp()
+hero.moveRight()
+hero.say(password)
+
+# A variable changes its value whenever you assign it.
+password = 'So Many Doors'
+hero.moveRight()
+# Change the string in this line to the password variable.
+hero.say(password) # ∆ Change this!
+
+password = 'Let Me Out Of Here'
+# Move to the last door and say the password variable to open it.
+hero.moveRight()
+hero.say(password)
