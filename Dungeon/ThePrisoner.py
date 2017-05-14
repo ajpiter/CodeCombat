15beb7dc950f08d81d68ea322c20a6d4d203d761
a-piter <amanda.piter@gmail.com>
2017-05-14 17:52:02 -0400

    Create ThePrisoner.py

---
+++ Dungeon/ThePrisoner.py
@@ -0,0 +1,12 @@
+# Free Patrick from behind the "Weak Door".
+hero.moveRight()
+hero.attack("Weak Door")
+
+# Defeat the guard, named "Two".
+hero.moveRight(2)
+hero.moveDown(2)
+hero.attack("Two")
+hero.attack("Two")
+# Get the gem.
+hero.moveDown()
+hero.moveRight()
