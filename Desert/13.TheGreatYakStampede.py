f05bc64ba01f2e9e7cd18ff102bd16867af920c0
a-piter <amanda.piter@gmail.com>
2017-05-14 12:37:01 -0400

    Create 13.TheGreatYakStampede.py

---
+++ Desert/13.TheGreatYakStampede.py
@@ -0,0 +1,9 @@
+# Save the village by blocking the path of the incoming sand yak herd.
+# Move closer to the red X.
+# Build a fence at the red X.
+hero.moveXY(56, 19)
+hero.moveXY(37, 24)
+hero.moveXY(12, 31)
+hero.moveXY(12, 51)
+hero.moveXY(35, 54)
+hero.buildXY("fence", 39, 53)
