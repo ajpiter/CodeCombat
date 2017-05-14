c7f474318c0a9ad00fd362ef83fb1555a5c7d7c6
a-piter <amanda.piter@gmail.com>
2017-05-14 18:07:25 -0400

    Create FireDancing.py

---
+++ Dungeon/FireDancing.py
@@ -0,0 +1,8 @@
+# Code normally executes in the order it's written.
+# Loops repeat a block of code multiple times.
+# Use tab or 4 spaces to indent the move lines under the loop.
+
+while True:
+    hero.moveRight()
+    # Add a moveLeft command to the loop here
+    hero.moveLeft()
