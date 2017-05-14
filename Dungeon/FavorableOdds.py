d0e8d858c935f4724e1a80c23a34aa9b73bc8cff
a-piter <amanda.piter@gmail.com>
2017-05-14 17:50:56 -0400

    Create FavorableOdds.py

---
+++ Dungeon/FavorableOdds.py
@@ -0,0 +1,9 @@
+# Attack both ogres and grab the gem.
+hero.moveRight()
+hero.attack("Krug")
+hero.attack("Krug")
+hero.moveRight()
+hero.moveUp()
+hero.attack("Grump")
+hero.attack("Grump")
+hero.moveLeft(2)
