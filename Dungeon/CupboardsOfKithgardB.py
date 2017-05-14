acf4370f96fc825309fd23bb516d5d693d1f94ef
a-piter <amanda.piter@gmail.com>
2017-05-14 18:06:42 -0400

    Create CupboardsOfKithgardB.py

---
+++ Dungeon/CupboardsOfKithgardB.py
@@ -0,0 +1,11 @@
+# There may be something around to help you!
+
+# First, move to the Cupboard.
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
+hero.moveDown(2)
+
+# Then, attack the "Cupboard" inside a while-true loop.
+while True:
+    hero.attack("Cupboard")
