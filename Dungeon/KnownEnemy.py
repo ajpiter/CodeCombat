37b55cdcea4f96b034768942ebadc268d99c2d09
a-piter <amanda.piter@gmail.com>
2017-05-14 18:29:06 -0400

    Create KnownEnemy.py

---
+++ Dungeon/KnownEnemy.py
@@ -0,0 +1,14 @@
+# You can use a variable like a nametag.
+
+enemy1 = "Kratt"
+enemy2 = "Gert"
+enemy3 = "Ursa"
+
+hero.attack(enemy1)
+hero.attack(enemy1)
+
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+hero.attack(enemy3)
+hero.attack(enemy3)
