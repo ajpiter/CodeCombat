cf8d348c9e636c2d6dd84d8b47ffe01279e6a3b1
a-piter <amanda.piter@gmail.com>
2017-05-14 19:02:29 -0400

    Create TheRaisedSword.py

---
+++ Dungeon/TheRaisedSword.py
@@ -0,0 +1,9 @@
+# Defeat the ogres.
+# Remember that they each take two hits.
+
+hero.attack("Rig")
+hero.attack("Rig")
+hero.attack("Gurt")
+hero.attack("Gurt")
+hero.attack("Ack")
+hero.attack("Ack")
