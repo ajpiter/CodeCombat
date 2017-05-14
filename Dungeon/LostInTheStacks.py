1a3c450e529417bb5dc7fdc74b5292a4fd371d5d
a-piter <amanda.piter@gmail.com>
2017-05-14 17:54:13 -0400

    Create LostInTheStacks.py

---
+++ Dungeon/LostInTheStacks.py
@@ -0,0 +1,11 @@
+# Loops repeat a block of code multiple times.
+# A block of code is INDENTED 4 spaces
+
+# A code block starts with a colon :
+while True:
+    # The movement lines need to be indented!
+    # Add 4 SPACES to the beginning of each line under while True:
+    hero.moveRight()
+    hero.moveUp() # ∆ INDENT this line
+    hero.moveLeft() # ∆ INDENT this line
+    hero.moveDown() # ∆ INDENT this line
