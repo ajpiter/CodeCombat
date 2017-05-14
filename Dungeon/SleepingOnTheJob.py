4f536429b465a9ac3ca467c80aff7b068dfb7413
a-piter <amanda.piter@gmail.com>
2017-05-14 17:49:28 -0400

    Create SleepingOnTheJob.py

---
+++ Dungeon/SleepingOnTheJob.py
@@ -0,0 +1,13 @@
+# The guards protect the buttons to the door.
+
+# Move between the sleeping ogres:
+hero.moveUp()
+hero.moveRight(2)
+
+# Defeat the ogres by attacking them twice:
+hero.attack("Vera")
+hero.attack("Vera")
+hero.attack("Bas")
+hero.attack("Bas")
+# Move to the gem and escape:
+hero.moveRight()
