eaaa165dcc08299789cf3e11316bc86242b83766
a-piter <amanda.piter@gmail.com>
2017-05-14 12:41:38 -0400

    Create 14.Operation'Killdear'.py

---
+++ Desert/14.Operation'Killdear'.py
@@ -0,0 +1,20 @@
+# Lure the ogres into a trap. These ogres are careful.
+# They will only follow if the hero is injured.
+
+# This function checks the hero's health 
+# and returns a Boolean value.
+def shouldRun():
+    if hero.health < hero.maxHealth / 2:
+        return True
+    else:
+        return False
+
+while True:
+    # Move to the X only if shouldRun() returns True
+    if shouldRun(True):
+        hero.moveXY(75, 37)
+    # Else, attack!
+    else:
+        enemy = hero.findNearestEnemy()
+        if enemy:
+            hero.attack(enemy)
