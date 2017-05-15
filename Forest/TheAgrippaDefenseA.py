b7d6b5b32cecfab0d8f3ccbe62408187827f575b
AJ Piter <amanda.piter@gmail.com>
2017-05-15 14:03:15 -0400

    Create TheAgrippaDefenseA.py

---
+++ Forest/TheAgrippaDefenseA.py
@@ -0,0 +1,16 @@
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        # Replace this with your own code.
+        # Find the distance to the enemy with distanceTo.
+        if hero.distanceTo(enemy) < 5:
+            if hero.isReady("cleave"):
+                hero.cleave(enemy)
+            else:
+                hero.attack(enemy)
+        
+        # If the distance is less than 5 meters...
+        
+            # ... if "cleave" is ready, cleave!
+            
+            # ... else, just attack.
