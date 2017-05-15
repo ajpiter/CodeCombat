8b7c43bea2decc08bc4c2be73a9d34155f8ec3c3
AJ Piter <amanda.piter@gmail.com>
2017-05-15 13:44:04 -0400

    Create RangeFinder.py

---
+++ Forest/RangeFinder.py
@@ -0,0 +1,18 @@
+# Ogres are scouting the forest!
+# Use the distanceTo method to find where the enemies are.
+# Say the distance for each enemy to tell the artillery where to fire!
+
+enemy1 = "Gort"
+distance1 = hero.distanceTo(enemy1)
+hero.say(distance1)
+
+enemy2 = "Smasher"
+distance2 = hero.distanceTo(enemy2)
+# Say the distance2 variable!
+hero.say(distance2)
+# Find and say the distance to the rest of the enemies:
+# Don't shoot at your friends!
+
+enemy4 = "Gorgnub"
+distance4 = hero.distanceTo(enemy4)
+hero.say(distance4)
