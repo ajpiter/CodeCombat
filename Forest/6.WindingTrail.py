3aa05d9b5fc97a9c350d02cc8a8e5f726562b5e8
AJ Piter <amanda.piter@gmail.com>
2017-05-15 12:58:13 -0400

    Create 6.WindingTrail.py

---
+++ Forest/6.WindingTrail.py
@@ -0,0 +1,10 @@
+# Go to the end of the path and build a fence on the X
+# Use your moveXY(x, y) function.
+
+# It's the first point of the path.
+hero.moveXY(36, 59)
+# Move at the next points of the path.
+hero.moveXY(37, 13)
+hero.moveXY(71, 19)
+# Build a fence to stop the ogre.
+hero.buildXY("fence", 72, 24)
