6eb7ede3031679bbb8c4fc60671e48b79318b5ef
AJ Piter <amanda.piter@gmail.com>
2017-05-15 13:00:30 -0400

    Create 8.BackwoodsAmbush.py

---
+++ Forest/8.BackwoodsAmbush.py
@@ -0,0 +1,26 @@
+hero.moveXY(24, 42)
+enemy = hero.findNearestEnemy()
+if enemy:
+    hero.attack(enemy)
+    hero.attack(enemy)
+
+hero.moveXY(27, 60)
+enemy = hero.findNearestEnemy()
+if enemy:
+    hero.attack(enemy)
+    hero.attack(enemy)
+    pass # pass is a placeholder.
+
+hero.moveXY(42, 50)
+enemy = hero.findNearestEnemy()
+if enemy:
+    hero.attack(enemy)
+    hero.attack(enemy)
+    pass
+
+hero.moveXY(39, 24)
+enemy = hero.findNearestEnemy()
+if enemy:
+    hero.attack(enemy)
+    hero.attack(enemy)
+    pass
