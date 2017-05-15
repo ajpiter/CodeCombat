e464852f54a5a421a153f657a4aa8069d273db38
AJ Piter <amanda.piter@gmail.com>
2017-05-15 12:58:57 -0400

    Create 7.ForrestEvasion.py

---
+++ Forest/7.ForrestEvasion.py
@@ -0,0 +1,11 @@
+# There is a headhunter in the area!
+# Move around the forest to avoid his line-of-sight.
+
+while True:
+    hero.moveXY(56,44)
+    hero.moveXY(40,56)
+    # Use moveXY to keep moving around the forest to survive.
+    hero.moveXY(24, 44)
+    hero.moveXY(24, 25)
+    hero.moveXY(40, 12)
+    hero.moveXY(56, 24)
