01db3deb1e883d20fca3ab7f79739be8465bc02c
AJ Piter <amanda.piter@gmail.com>
2017-05-15 15:03:49 -0400

    Create UnusualProblem.py

---
+++ Forest/UnusualProblem.py
@@ -0,0 +1,14 @@
+# Defeat munchkins, collect coins. Everything as usual.
+# Use AND to check existence and type in one statement.
+
+while True:
+    enemy = hero.findNearestEnemy()
+    # With AND, the type is only checked if enemy exists.
+    if enemy and enemy.type == "munchkin":
+        hero.attack(enemy);
+    # Find the nearest item.
+    item = hero.findNearestItem()
+    
+    # Collect item if it exists and its type is "coin".
+    if item and item.type == "coin":
+        hero.moveXY(item.pos.x, item.pos.y)
