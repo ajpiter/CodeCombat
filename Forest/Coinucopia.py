0df2411cab9d1a0ecaf0afed4461f26083343506
AJ Piter <amanda.piter@gmail.com>
2017-05-15 16:27:14 -0400

    Create Coinucopia.py

---
+++ Forest/Coinucopia.py
@@ -0,0 +1,8 @@
+# Press Submit when you are ready to place flags.
+# Flag buttons appear in the lower left after pressing Submit.
+while True:
+    flag = hero.findFlag()
+    if flag:
+        hero.pickUpFlag(flag)
+    else:
+        hero.say("Place a flag for me to go to.")
