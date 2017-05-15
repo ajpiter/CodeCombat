1a58e6e69020a40c5f7a6c6141f2727fde880e76
AJ Piter <amanda.piter@gmail.com>
2017-05-15 15:48:02 -0400

    Create RewardAndRuination.py

---
+++ Forest/RewardAndRuination.py
@@ -0,0 +1,15 @@
+# It seems like the Ogre Chieftain is stealing your gems!
+# Use the two artillery cannons to defeat your enemies and gather the gems.
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        enemyPos = enemy.pos.x + " " + enemy.pos.y
+        hero.say("Enemy at " + enemyPos)
+        
+    # Now that you have sweet revenge, why not have your cake and eat it, too?
+    # Find the item's position and say it for your artillery to target.
+    item = hero.findNearestItem()
+    if item:
+        itemPos = item.pos.x + " " + item.pos.y
+        hero.say("Item at "+ itemPos)
