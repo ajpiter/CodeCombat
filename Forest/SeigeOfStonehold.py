26df1042990617d99b0e17bb58a4d00f1fe58649
AJ Piter <amanda.piter@gmail.com>
2017-05-15 16:41:06 -0400

    Create SeigeOfStonehold.py

---
+++ Forest/SeigeOfStonehold.py
@@ -0,0 +1,10 @@
+"""There is no reasonable explation to why this allows me to successfully complete the level but it does. You will most likely have to use an if statment to move to the X if you are weak, and use flags to attack and retreat from the enemy. Good Luck"""
+
+
+# Help your friends beat the minions that Thoktar sends against you.
+# You'll need great equipment and strategy to win.
+# Flags might help, but it's up to you–be creative!
+# There is a doctor behind the fence. Move to the X to get healed!
+
+enemy = hero.findNearestEnemy()
+hero.moveXY(55, 14)
