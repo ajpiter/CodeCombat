ff6bb87076b3d3dd89b0ef3c3dcf4f84942df134
AJ Piter <amanda.piter@gmail.com>
2017-05-15 15:40:51 -0400

    Create StarShower.py

---
+++ Forest/StarShower.py
@@ -0,0 +1,12 @@
+# Pick up coins only if they are closer than 20m.
+# Pick up all gems.
+
+while True:
+    item = hero.findNearestItem()
+    distance = hero.distanceTo(item)
+    # If the item's type is "gem"
+    # OR the distance to the item less than 20 meters:
+    if item.type == "gem" or distance < 20:
+        hero.moveXY(item.pos.x, item.pos.y)
+    # Get the item!
+        # Move to item's position.
