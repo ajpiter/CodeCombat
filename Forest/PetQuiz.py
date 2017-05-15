e9e822f080ed8a79de6303c92a369ccee4cbe111
AJ Piter <amanda.piter@gmail.com>
2017-05-15 14:49:34 -0400

    Create PetQuiz.py

---
+++ Forest/PetQuiz.py
@@ -0,0 +1,16 @@
+# Write an event handler function called onHear
+
+# Define the onHear function
+def onHear():
+    pet.say("meow")
+    # The pet should say something in onHear.
+    
+
+pet.on("hear", onHear)
+
+hero.say("Do you understand me?")
+hero.say("Are you a cougar?")
+hero.say("How old are you?")
+# Ask two more questions.
+hero.say("Hi")
+hero.say("bye")
