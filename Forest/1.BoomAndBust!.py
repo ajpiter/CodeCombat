eccb308c5c8e2ae10c77c81b10bfbd76c8b54da4
AJ Piter <amanda.piter@gmail.com>
2017-05-15 12:53:45 -0400

    Create 1.BoomAndBust!.py

---
+++ Forest/1.BoomAndBust!.py
@@ -0,0 +1,8 @@
+# Use your buildXY hammer to build two "fire-trap"s near the gate.
+# They will detonate when you move back to a safe distance!
+# Then, make a run for the forest!
+
+hero.buildXY("fire-trap", 35, 35)
+hero.buildXY("fire-trap", 35, 30)
+hero.moveLeft()
+hero.moveRight(2)
