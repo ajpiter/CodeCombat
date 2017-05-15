422f638feb0068806f765a61128326ab08588284
AJ Piter <amanda.piter@gmail.com>
2017-05-15 16:23:05 -0400

    Create LogicalPath.py

---
+++ Forest/LogicalPath.py
@@ -0,0 +1,29 @@
+# Get two secret true/false values from the wizard.
+hero.moveXY(14, 24)
+secretA = hero.findNearestFriend().getSecretA()
+secretB = hero.findNearestFriend().getSecretB()
+
+# If BOTH secretA and secretB are true, take the high path; otherwise, take the low path.
+# Check the guide for notes on how to write logical expressions.
+secretC = secretA and secretB
+if secretC:
+    hero.moveXY(20, 33)
+else:
+    hero.moveXY(20, 15)
+hero.moveXY(26, 24)
+
+# If EITHER secretA or secretB is true, take the high path.
+secretC = secretA or secretB
+if secretC: 
+    hero.moveXY(32, 33)
+else:
+    hero.moveXY(32, 15)
+hero.moveXY(38, 24)
+
+# If secretB is NOT true, take the high path.
+secretC =not secretC 
+if secretC: 
+    hero.moveXY(44, 33)
+else:
+    hero.moveXY(44, 15)
+hero.moveXY(50, 24)
