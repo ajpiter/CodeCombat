65ba9f03260c430279c6460933d95556d4f9ae3a
AJ Piter <amanda.piter@gmail.com>
2017-05-15 12:54:27 -0400

    Create 2.DefenseOfPlainswood.py

---
+++ Forest/2.DefenseOfPlainswood.py
@@ -0,0 +1,6 @@
+# Build two fences on the marks to keep the villagers safe!
+# Hover your mouse over the world to get X,Y coordinates.
+hero.buildXY("fence", 41, 51)
+hero.buildXY("fence", 37, 51)
+hero.buildXY("fence", 41, 20)
+hero.buildXY("fence", 37, 20)
