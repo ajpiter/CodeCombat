069869210d6be6a3f57470498ed08e6e0404c427
AJ Piter <amanda.piter@gmail.com>
2017-05-18 13:15:51 -0400

    Create MushroomNoise.py

---
+++ Desert/MushroomNoise.py
@@ -0,0 +1,24 @@
+# Defeat the skeleton and open the chest.
+
+def onSpawn (event):
+    # Pet should find the health potion (type is "potion"): 
+    potion = pet.findNearestByType('potion')
+    # and fetch it:
+    if potion: 
+        pet.fetch(potion)
+    # Pet should find the gold key (type is "gold-key"):
+    goldkey = pet.findNearestByType('gold-key')
+    # and fetch it:
+    if goldkey:
+        pet.fetch(goldkey)
+    pass
+
+# Pet can find more than just items:
+skeleton = pet.findNearestByType("skeleton")
+pet.on("spawn", onSpawn)
+
+while True:
+    if skeleton.health > 0:
+        hero.attack(skeleton)
+    else:
+        hero.moveXY(31, 38)
