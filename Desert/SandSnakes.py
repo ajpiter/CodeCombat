e678f6a7b3c7538ee22487816896acf17de5729b
AJ Piter <amanda.piter@gmail.com>
2017-05-30 15:43:56 -0400

    Create SandSnakes.py

---
+++ Desert/SandSnakes.py
@@ -0,0 +1,22 @@
+# This field is covered in firetraps.  Thankfully we've sent a scout ahead to find a path.  He left coins along the path so that if we always stick to the nearest coin, we'll avoid the traps.
+
+# This canyon seems to interfere with your findNearest glasses!
+# You'll need to find the nearest coins on your own.
+while True:
+    coins = hero.findItems()
+    coinIndex = 0
+    nearest = None
+    nearestDistance = 9999
+    # Loop through all the coins to find the nearest one.
+    while coinIndex < len(coins):
+        coin = coins[coinIndex]
+        coinIndex += 1
+        distance = hero.distanceTo(coin)
+        # If this coin's distance is less than the nearestDistance
+        if distance < nearestDistance:
+            # Set nearest to coin
+            nearest = coin
+            # Set nearestDistance to distance
+            nearestDistance = distance
+    hero.moveXY(nearest.pos.x, nearest.pos.y)
+    # If there's a nearest coin, move to its position. You'll need moveXY so you don't cut corners and hit a trap.
