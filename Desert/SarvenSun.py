612ef7450a094e5993c27dce28808e98025ec2b6
AJ Piter <amanda.piter@gmail.com>
2017-07-13 14:13:46 -0400

    Create SarvenSun.py

---
+++ Desert/SarvenSun.py
@@ -0,0 +1,13 @@
+# To disable the fire-traps add the lowest trap.value to the highest value.
+# Move to the white X and say the answer to Kitty the cougar.
+# Defeat all the ogres if you dare.
+# Once all ogres are defeated move to the red X.
+# Look out for potions to boost your health.
+
+whiteX = {'x':27, 'y':42}
+
+redX = {'x':151 , 'y': 116}
+
+hero.moveXY(28, 42)
+hero.say("124")
+hero.moveXY(151, 116)
